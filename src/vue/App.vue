ab49c46f3a59432df484e593d68e309dd6615ef6
Jorge Moreno <alterebro@gmail.com>
2020-05-20 20:24:10 +0200

    Styling and layout structure

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,16 +1,32 @@
 <template>
     <div class="app">
-        <div v-show="!counter.start">
-            <ul>
-                <li v-for="(item, i) in items">{{ item.count }} - {{ item.start }} - {{ item.end }} <button @click="itemRemove(i)">remove</button> </li>
-            </ul>
-            <button @click="counterStart()">start!</button>
-        </div>
-        <div v-show="counter.start">
-            <button @click="counterIncrease()">{{ counter.count }}</button>
-            <button @click="counterStop()">Stop &amp; Save</button>
-            <button @click="counterReset()">Cancel</button>
-        </div>
+        <header>
+            <h1>{{ appName }}</h1>
+        </header>
+        <main>
+            <section v-show="!counter.start">
+                <ul>
+                    <li v-for="(item, i) in items">{{ item.count }} - {{ item.start }} - {{ item.end }} <button @click="itemRemove(i)">remove</button> </li>
+                </ul>
+                <button @click="counterStart()">start!</button>
+            </section>
+            <section v-show="counter.start">
+                <button @click="counterIncrease()">{{ counter.count }}</button>
+                <button @click="counterStop()">Stop &amp; Save</button>
+                <button @click="counterReset()">Cancel</button>
+            </section>
+            <section v-show="view.settings">
+                <h2>Settings</h2>
+                <ul>
+                    <li>Export data / Save DB</li>
+                    <li>Import DB</li>
+                </ul>
+            </section>
+        </main>
+        <footer>
+            <hr />
+            <p>{{ appName }} v{{ appVersion }} by <a href="https://twitter.com/alterebro">@alterebro</a></p>
+        </footer>
     </div>
 </template>
 
@@ -19,6 +35,10 @@ const App = {
     data() {
         return {
             appName: 'Counter App',
+            appVersion : '0.0.1',
+            view: {
+                settings : false
+            },
 
             counter : {
                 count : 0,
@@ -69,17 +89,79 @@ export default App;
 </script>
 
 <style lang="scss">
-$primary-color: white;
-$bg: black;
 
-html, body {
+$colorBg : #f7f7f7;
+$colorFg : #303030;
+$colorAlpha : #0062ee;
+$colorBeta : #03dac6;
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+html, body, h1, h2, h3, h4, p, ul, li {
     margin: 0;
     padding: 0;
 }
-
-h1 {
-  color: $primary-color;
-  background-color: $bg;
-  margin: 0;
+li { list-style-type: none; }
+article, footer, header, main, section {
+	display: block;
 }
+button {
+    font: inherit;
+	color: inherit;
+	margin: 0;
+    padding: 0;
+    text-transform: none;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+html {
+    line-height: 1;
+	text-size-adjust: 100%;
+	box-sizing: border-box;
+	font-size: 62.5%;
+	word-break: break-word;
+    cursor: default;
+}
+body {
+    font-family: "Roboto", -apple-system, BlinkMacSystemFont, "Segoe UI", "Droid Sans", "Helvetica Neue", Arial, sans-serif;
+    font-size: 1.3em;
+    font-weight: 400;
+    line-height: 1.6;
+    background: $colorBg;
+    color: $colorFg;
+}
+html, body {
+    height: 100%;
+    min-height: 100%;
+    overflow: hidden;
+}
+*, *:before, *:after {
+	box-sizing: inherit;
+	background-repeat: no-repeat;
+}
+a {
+    color: $colorAlpha;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+header {
+    padding: 1rem;
+    color: $colorBg;
+    background: $colorAlpha;
+
+    h1 {
+      font-weight: 400;
+    }
+}
+
+main {
+    padding: 1rem;
+}
+
+footer {
+    padding: 1rem;
+}
+
 </style>
