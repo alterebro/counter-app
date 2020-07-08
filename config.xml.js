94b39c24f14ece271009d1a572f167889a287910
Jorge Moreno <alterebro@gmail.com>
2020-07-08 20:28:22 +0200

    Autogenerate config.xml

---
+++ config.xml.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const pkg = require('./package.json');
+const builder = require('xmlbuilder');
+
+const root = builder.create('widget')
+    .att('xmlns', 'http://www.w3.org/ns/widgets')
+    .att('xmlns:gap', 'http://phonegap.com/ns/1.0')
+    .att('id', 'com.alterebro.counter-app')
+    .att('version', pkg.version);
+
+    const _name = root.ele('name', 'Counter App');
+    const _desc = root.ele('description', pkg.description);
+    const _author = root.ele('author', { 'href' : pkg.author.url }, pkg.author.name);
+    const _icon = root.ele('icon', {'src' : 'counter-app-icon.png'});
+    const _platform = root.ele('platform', { 'name' : 'android'}, '');
+
+const xml = root.end({ pretty: true});
+
+fs.writeFileSync('./static/config.xml', xml);
+console.log('config.xml file created!');
